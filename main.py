571eae6dd012aa8937eae462f96ad34a70cab441
xpz12 <141086740+xpz12@users.noreply.github.com>
2024-08-09 11:15:58 +0000

    readmemd edit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import keyboard
 import pyperclip
 
+# Чтобы изменить копируемый текст на желаемый ,вставьте свой вместо Text 1.
 texts = {
     '1': 'Text 1',
     '2': 'Text 2',
@@ -15,10 +16,12 @@ def copy_text(text_key):
         pyperclip.copy(text)
         print(f'Copied: {text}')
 
+# Чтобы изменить хоткей ,вставьте свой вместо alt+1.
 keyboard.add_hotkey('alt+1', copy_text, args=('1',))
 keyboard.add_hotkey('alt+2', copy_text, args=('2',))
 keyboard.add_hotkey('alt+3', copy_text, args=('3',))
 keyboard.add_hotkey('alt+4', copy_text, args=('4',))
 keyboard.add_hotkey('alt+5', copy_text, args=('5',))
 
+# Программа работает бесконечно пока не нажмете esc
 keyboard.wait('esc')
